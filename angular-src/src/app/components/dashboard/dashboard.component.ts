a41f11fdcb951211e80db86ac16ad1ced9ca0c7a
Katrina Smith <ksmith10@ggc.edu>
2017-03-29 01:17:10 -0400

    created dashboard flashcard set, view btn linked to view-flashset page. view-flash-card displays 1 flashcard, binding to term/definition

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -3,19 +3,23 @@ import { FlashcardService } from '../../services/flashcard.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
 
 
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
   moduleId: module.id
-  
 
 })
 export class DashboardComponent implements OnInit {
   title = "Dashboard";
- 
   
+  flashcardSetName =[
+    {user: 'Katrina', subject: 'Calculus Ch1'},
+    {user: 'Katrina', subject: 'Calculus Ch2'},
+    {user: 'Katrina', subject: 'Principles of Management Ch1'},
+    {user: 'Katrina', subject: 'Algebra Fundamentals Ch6'}
+    ];
+ 
   constructor(
         private flashcardService: FlashcardService,
         private flashMessage: FlashMessagesService
