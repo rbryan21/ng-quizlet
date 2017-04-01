7b73a1ef3f59fe49309d482cedb34841ee9e3bde
Robert <rbryan21@gmail.com>
2017-04-01 00:13:46 -0400

    Card's populating!

--- angular-src/src/app/components/view-flashcard-set/view-flashcard-set.component.ts
+++ angular-src/src/app/components/view-flashcard-set/view-flashcard-set.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate, HostListener } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 
 import { FlashcardService } from '../../services/flashcard.service';
@@ -7,10 +7,63 @@ import { FlashcardService } from '../../services/flashcard.service';
 @Component({
   selector: 'app-view-flashcard-set',
   templateUrl: './view-flashcard-set.component.html',
-  styleUrls: ['./view-flashcard-set.component.css']
+  styleUrls: ['./view-flashcard-set.component.css'],
+  animations: [
+      trigger('flipState', [
+        state('active', style({
+          transform: 'rotateY(179.9deg)'
+        })),
+        state('inactive', style({
+          transform: 'rotateY(0)'
+        })),
+        transition('active => inactive', animate('500ms ease-out')),
+        transition('inactive => active', animate('500ms ease-in'))
+      ])  
+    ]
 })
 export class ViewFlashcardSetComponent implements OnInit {
-  flashcardSet = [];
+  flashcards = [];
+  name;
+  currentCardIndex = 0;
+
+  @HostListener('document:keydown') onkeyup() {
+    this.onKey(event);
+  }
+
+  flip: string = 'inactive';
+
+  onKey(event: any) { 
+    console.log(this.flashcards.length);
+    console.log(event);
+    switch (event.key) {
+      case 'ArrowRight':
+        if (this.currentCardIndex < this.flashcards.length-1) {
+          this.currentCardIndex++;
+          console.log('currentCardIndex = ', this.currentCardIndex);
+        }
+        break;
+      case 'ArrowLeft':
+        if (this.currentCardIndex > this.flashcards.length-2) {
+          this.currentCardIndex--;
+          console.log('currentCardIndex = ', this.currentCardIndex);
+        }
+        break;
+      case 'ArrowUp':
+      case 'ArrowDown':
+            this.toggleFlip();
+            break;
+      default:
+        break;
+    }
+  }
+  
+  toggleFlip() {
+    this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
+  }
+
+  currentCard() {
+    return 1;
+  }
 
   constructor(
     private flashcardService: FlashcardService,
@@ -19,13 +72,16 @@ export class ViewFlashcardSetComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    console.log('currentCardIndex = ', this.currentCardIndex);
     this.route.params.forEach((params: Params) => {
           let id = params['id'];
           console.log('idd = ', id);
           this.flashcardService.getFlashcardSet(id).subscribe(data => {
             if (data.success) {
-              console.log(data);
-              this.flashcardSet = data.flashcardSet; 
+              // console.log(data.flashcardSet.flashcards[0].term);
+              this.name = data.flashcardSet.name;
+              this.flashcards = data.flashcardSet.flashcards; 
+              console.log('flashcards = ', this.flashcards);
             } else {
               let link = ['/dashboard'];
               this.router.navigate(link);
